1ea667f3e8c2845409334aff9c3b4054d5b8bc3e
wto <qiushimao@hotmail.com>
2017-09-23 19:49:16 +0800

    [RN] update

--- index.android.js
+++ index.android.js
@@ -10,52 +10,48 @@ import {
     StyleSheet,
     Text,
     View,
-    TextInput,
-    Button,
-    Alert
+    SectionList,
 } from 'react-native';
-const onButtonPress = () => {
-    Alert.alert('Button has been pressed!');
-};
 
 export default class JEEK_Client extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    _renderItem = (info) => {
+        var txt = '  ' + info.item.title;
+        return <Text
+            style={{ height: 60, textAlignVertical: 'center', backgroundColor: "#ffffff", color: '#5C5C5C', fontSize: 15 }}>{txt}</Text>
+    };
+
+    _sectionComp = (info) => {
+        var txt = info.section.key;
+        return <Text
+            style={{ height: 50, textAlign: 'center', textAlignVertical: 'center', backgroundColor: '#6b80e3', color: 'white', fontSize: 30 }}>{txt}</Text>
+    };
+
     render() {
+        var sections = [
+            { key: "HTML", data: [{ title: "HTML" }] },
+            { key: "CSS", data: [{ title: "CSS" }]},
+            { key: "JavaScript", data: [{ title: "JavaScript" }] },
+            { key: "Node.js", data: [{ title: "Node.js" }] },
+            { key: "Database", data: [{ title: "Database" }] },
+            { key: "Philosophy", data: [{ title: "Philosophy" }] },
+        ];
+
         return (
-            <View style={styles.container}>
-                <Text style={styles.welcome}>
-                    JEEK
-                </Text>
-                <Text>
-                    建平弹幕系统（测试中）
-                </Text>
-                <TextInput
-                    style={{height: 50,width: 150}}
-                    placeholder="Type here to translate!"
-                    onChangeText={(text) => this.setState({text})}
-                />
-                {/*<Text style={{padding: 10, fontSize: 42}}>*/}
-                    {/*{this.state.text.split(' ').map((word) => word && '🍕').join(' ')}*/}
-                {/*</Text>*/}
-                <Button
-                    onPress={onButtonPress}
-                    title="发送弹幕"
+            <View style={{ flex: 1 }}>
+                <SectionList
+                    renderSectionHeader={this._sectionComp}
+                    renderItem={this._renderItem}
+                    sections={sections}
+                    ItemSeparatorComponent={() => <View><Text></Text></View>}
                 />
             </View>
         );
     }
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF',
-    },
-    welcome: {
-        marginBottom: 20,
-        fontSize: 40
-    }
-});
 
 AppRegistry.registerComponent('JEEK_Client', () => JEEK_Client);
